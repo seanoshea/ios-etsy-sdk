a6f5a8c8171cd51ddd7d919d00f379bf1fea97b3
seanoshea <oshea.ie@gmail.com>
2013-01-25 12:41:57 -0800

    Licensing.

--- ios-etsy-sdk/ios-etsy-sdk/SOSEtsyListingsResult.h
+++ ios-etsy-sdk/ios-etsy-sdk/SOSEtsyListingsResult.h
@@ -1,5 +1,5 @@
 /*
- Copyright 2012 Sean O' Shea
+ Copyright 2012 - 2013 Sean O' Shea
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@
 /**
  `SOSEtsyListingsResult` is returned as a result of making a SOSEtsyListingsRequest.
  */
-
 @interface SOSEtsyListingsResult : SOSEtsyResult
 
 @end
