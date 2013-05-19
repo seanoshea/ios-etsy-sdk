3c3b329f1e1e3bce7032e9f7234ba857b634bcda
seanoshea <oshea.ie@gmail.com>
2013-05-19 09:36:55 +0100

    Permission issues?

--- ios-etsy-sdk/travis/before_script.sh
+++ ios-etsy-sdk/travis/before_script.sh
@@ -6,6 +6,3 @@ xcodebuild -list -workspace ios-etsy-sdk/ios-etsy-sdk.xcodeproj
 
 brew update
 brew install xctool ios-sim
-IOS_SIM_DIR=`brew info ios-sim | sed -n '3 p' | awk '{print $1}'`
-mkdir bin
-cp $IOS_SIM_DIR/bin/ios-sim ./bin/ios-sim
