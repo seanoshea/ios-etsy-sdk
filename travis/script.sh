879b4b640c6affe5f42f6a7bfdbdb79624503320
seanoshea <oshea.ie@gmail.com>
2013-05-19 12:53:51 +0100

    Editing the scheme to include kiwi.

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -project ios-etsy-sdkTests/Dependencies/Kiwi/Kiwi.xcodeproj -scheme Kiwi build
+# xctool -project ios-etsy-sdkTests/Dependencies/Kiwi/Kiwi.xcodeproj -scheme Kiwi build
 xctool -project ios-etsy-sdk.xcodeproj -scheme ios-etsy-sdk build test
