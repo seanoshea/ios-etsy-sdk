bbf553d6fbc67ed2044cf65086deec2556a4b87b
seanoshea <oshea.ie@gmail.com>
2013-06-03 22:08:41 +0100

    travis again.

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-cd bootstrap
-xctool -workspace ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk test
+xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk test
