9bddea7bb11d1a5d0311e306d3bac4f9886b06e9
seanoshea <oshea.ie@gmail.com>
2013-06-04 22:59:07 +0100

    Explicitly calling out the Pods-ios-etsy-sdkTests build.

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch "i386" clean test
+xcodebuild -project bootstrap/Pods/Pods.xcodeproj -target Pods-ios-etsy-sdkTests -sdk iphonesimulator6.1 -configuration Debug -arch i386
+xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch i386 clean test
