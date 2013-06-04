485e6e1553edb84ef7bfa8cf86b1eddb06612da0
seanoshea <oshea.ie@gmail.com>
2013-06-04 23:04:45 +0100

    Skipping the clean as it could be removing the necessary lib file.

--- travis/script.sh
+++ travis/script.sh
@@ -2,4 +2,4 @@
 set -e
 
 xcodebuild -project bootstrap/Pods/Pods.xcodeproj -target Pods-ios-etsy-sdkTests -sdk iphonesimulator6.1 -configuration Debug -arch i386
-xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch i386 clean test
+xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch i386 test
