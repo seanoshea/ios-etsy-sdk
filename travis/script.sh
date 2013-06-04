3c04be86e91980345d2ed8616e5d95d68fee836b
seanoshea <oshea.ie@gmail.com>
2013-06-04 23:10:27 +0100

    Giving up on travis this evening.

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xcodebuild -project bootstrap/Pods/Pods.xcodeproj -target Pods-ios-etsy-sdkTests -sdk iphonesimulator6.1 -configuration Debug -arch i386
+# xcodebuild -project bootstrap/Pods/Pods.xcodeproj -target Pods-ios-etsy-sdkTests -sdk iphonesimulator6.1 -configuration Debug -arch i386
 xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch i386 test
