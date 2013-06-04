24b567a13ebdb7bdb83cf176cff3d156a8b185b0
seanoshea <oshea.ie@gmail.com>
2013-06-04 22:45:49 +0100

    Explicitly cleaning the project before a build.

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch "i386" test
+xctool -workspace bootstrap/ios-etsy-sdk.xcworkspace -scheme ios-etsy-sdk -sdk iphonesimulator6.1 -configuration Debug -arch "i386" clean test
